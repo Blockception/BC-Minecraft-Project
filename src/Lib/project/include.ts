a89aa7ebcec04430fb1bc6103874d875aa857842
DaanV2 <daanverstraten@hotmail.com>
2021-05-06 17:06:08 +0200

    Removed

--- src/Lib/project/include.ts
+++
@@ -1,5 +0,0 @@
-/*	Auto generated	*/
-export * as mcattributes from "./mcattributes/include";
-export * as mcdefinitions from "./mcdefinitions/include";
-export * as mcignore from "./mcignore/include";
-export * from "./project";
